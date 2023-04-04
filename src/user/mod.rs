9d7635c125abeda80b20361c37ada1a1e3ae9e87
Algys <algysrakhmetov@gmail.com>
2023-04-05 03:01:51 +0600

    update_pin function for User

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 enum BankId {
     One(u8),
     Two(u8),
@@ -11,4 +13,24 @@ struct User {
     pin: u16,
     balance: u128,
     last_transaction: i128,
+}
+
+impl User {
+    fn update_pin(&mut self) -> io::Result<()> {
+        let mut pin: String = String::new(); 
+        
+        print!("Enter new password: ");
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut pin)?;
+
+        match pin.trim().parse() {
+            Ok(num) => self.pin = num,
+            Err(_) => {
+                println!("Invalid password, please, try again.");
+                pin.clear();
+            }
+        }
+
+        Ok(())
+    }
 }
