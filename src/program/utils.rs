295308b9b76a389ad3b501a68704830e8ab89398
Algys <algysrakhmetov@gmail.com>
2023-04-05 02:52:09 +0600

    utility functions

---
+++ src/program/utils.rs
@@ -0,0 +1,8 @@
+fn run() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
