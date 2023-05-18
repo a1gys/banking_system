2a294d97b09625e51bbb7040297e4605bbeadde2
Algys <algysrakhmetov@gmail.com>
2023-05-18 17:50:58 +0600

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, process, error::Error};
 
+mod user;
 mod program;
 
 
 fn main() {
-    println!("Welcome to Bank\n");
+    println!("Welcome to Bank\n");    
 
     program::display_page();
-
-
 }
--- src/program/utils.rs
+++
@@ -1,8 +0,0 @@
-fn run() -> Result<(), Box<dyn Error>> {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
-    }
-    Ok(())
-}
